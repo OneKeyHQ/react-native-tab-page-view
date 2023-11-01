3f7964403e08b9e084c660b1d0cec19ae4f0ad4f
hublot <hublot@aliyun.com>
2023-11-01 15:53:58 +0800

    Initial

---
+++ index.ts
@@ -0,0 +1,6 @@
+import SelectedLabel from './src/SelectedLabel'
+import PageHeaderView from './src/PageHeaderView'
+import PageContentView from './src/PageContentView'
+import PageManager from './src/PageManager'
+
+export { SelectedLabel, PageHeaderView, PageContentView, PageManager }
---
+++ src/SelectedLabel/index.native.tsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import { requireNativeComponent, Animated, Platform, processColor } from 'react-native';
+
+const NativeSelectedLabel = requireNativeComponent('SelectedLabel')
+
+const AnimatedSelectedLabel = Animated.createAnimatedComponent(NativeSelectedLabel)
+
+export default AnimatedSelectedLabel
