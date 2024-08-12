a596f612c56f89f24468c6f1c45dbc391b87c9aa
hublot <hublot@aliyun.com>
2024-08-12 08:24:42 +0800

    fix: scroll gesture conflict

--- src/NestedTabView/index.native.tsx
+++ src/NestedTabView/index.native.tsx
@@ -1,3 +1,28 @@
-import { requireNativeComponent } from 'react-native';
+import { useRef } from 'react';
 
-export default requireNativeComponent('NestedTabView');
+import { Platform, requireNativeComponent } from 'react-native';
+
+const BaseNestedTabView = requireNativeComponent('NestedTabView');
+
+function NestedTabViewContainer(props: any) {
+  const point = useRef({ x: 0, y: 0 });
+  return (
+    // @ts-ignore
+    <BaseNestedTabView
+      onStartShouldSetResponderCapture={(e: any) => {
+        const { locationX: x, locationY: y } = e.nativeEvent;
+        point.current = { x, y };
+        return false;
+      }}
+      onMoveShouldSetResponderCapture={(e: any) => {
+        const { locationX: x, locationY: y } = e.nativeEvent;
+        return Platform.OS === 'android'
+          ? true
+          : Math.abs(x - point.current.x) > Math.abs(y - point.current.y);
+      }}
+      {...props}
+    />
+  );
+}
+
+export default NestedTabViewContainer;
